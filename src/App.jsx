9b3e1fb128962f171404cb495cd33df91570c0c5
Kalyani <17kalyani2001@gmail.com>
2024-07-06 20:35:07 +0530

    static design

---
+++ src/App.jsx
@@ -0,0 +1,180 @@
+import "./App.css";
+import addLogo from "./images/add.svg";
+import searchLogo from "./images/search.svg";
+import writeLogo from "./images/write.svg";
+import starLogo from "./images/star.svg";
+
+function App() {
+  return (
+    <div id="full">
+      <div id="frame">
+        <div id="side">
+          <div id="sidecontent">
+            <h3 id="name">
+              Flash
+              <br />
+              Notes
+            </h3>
+            <img id="addlogo" src={addLogo} alt="add logo" />
+          </div>
+        </div>
+        <div id="header">
+          <div>
+            <nav id="searchbar">
+              <img id="searchlogo" src={searchLogo} alt="search logo" />
+              Search
+            </nav>
+          </div>
+          <div id="notes">
+            <h1>Notes</h1>
+          </div>
+          <div id="cards">
+            <div id="card">
+              <div id="content">
+                <h4>Hii! This is me!</h4>
+                <img id="starlogo" src={starLogo} alt="star logo" />
+              </div>
+              <div id="foot">
+                <div>
+                  <h6>May 30, 2024</h6>
+                </div>
+                <div>
+                  <img id="writelogo" src={writeLogo} alt="wite logo" />
+                </div>
+              </div>
+            </div>
+            <div id="card">
+              <div id="content">
+                <h4>Hii! This is me!</h4>
+                <img id="starlogo" src={starLogo} alt="star logo" />
+              </div>
+              <div id="foot">
+                <div>
+                  <h6>May 30, 2024</h6>
+                </div>
+                <div>
+                  <img id="writelogo" src={writeLogo} alt="wite logo" />
+                </div>
+              </div>
+            </div>
+            <div id="card">
+              <div id="content">
+                <h4>Hii! This is me!</h4>
+                <img id="starlogo" src={starLogo} alt="star logo" />
+              </div>
+              <div id="foot">
+                <div>
+                  <h6>May 30, 2024</h6>
+                </div>
+                <div>
+                  <img id="writelogo" src={writeLogo} alt="wite logo" />
+                </div>
+              </div>
+            </div>
+            <div id="card">
+              <div id="content">
+                <h4>Hii! This is me!</h4>
+                <img id="starlogo" src={starLogo} alt="star logo" />
+              </div>
+              <div id="foot">
+                <div>
+                  <h6>May 30, 2024</h6>
+                </div>
+                <div>
+                  <img id="writelogo" src={writeLogo} alt="wite logo" />
+                </div>
+              </div>
+            </div>
+            <div id="card">
+              <div id="content">
+                <h4>Hii! This is me!</h4>
+                <img id="starlogo" src={starLogo} alt="star logo" />
+              </div>
+              <div id="foot">
+                <div>
+                  <h6>May 30, 2024</h6>
+                </div>
+                <div>
+                  <img id="writelogo" src={writeLogo} alt="wite logo" />
+                </div>
+              </div>
+            </div>
+
+            <div id="card">
+              <div id="content">
+                <h4>Hii! This is me!</h4>
+                <img id="starlogo" src={starLogo} alt="star logo" />
+              </div>
+              <div id="foot">
+                <div>
+                  <h6>May 30, 2024</h6>
+                </div>
+                <div>
+                  <img id="writelogo" src={writeLogo} alt="wite logo" />
+                </div>
+              </div>
+            </div>
+            <div id="card">
+              <div id="content">
+                <h4>Hii! This is me!</h4>
+                <img id="starlogo" src={starLogo} alt="star logo" />
+              </div>
+              <div id="foot">
+                <div>
+                  <h6>May 30, 2024</h6>
+                </div>
+                <div>
+                  <img id="writelogo" src={writeLogo} alt="wite logo" />
+                </div>
+              </div>
+            </div>
+            <div id="card">
+              <div id="content">
+                <h4>Hii! This is me!</h4>
+                <img id="starlogo" src={starLogo} alt="star logo" />
+              </div>
+              <div id="foot">
+                <div>
+                  <h6>May 30, 2024</h6>
+                </div>
+                <div>
+                  <img id="writelogo" src={writeLogo} alt="wite logo" />
+                </div>
+              </div>
+            </div>
+            <div id="card">
+              <div id="content">
+                <h4>Hii! This is me!</h4>
+                <img id="starlogo" src={starLogo} alt="star logo" />
+              </div>
+              <div id="foot">
+                <div>
+                  <h6>May 30, 2024</h6>
+                </div>
+                <div>
+                  <img id="writelogo" src={writeLogo} alt="wite logo" />
+                </div>
+              </div>
+            </div>
+            <div id="card">
+              <div id="content">
+                <h4>Hii! This is me!</h4>
+                <img id="starlogo" src={starLogo} alt="star logo" />
+              </div>
+              <div id="foot">
+                <div>
+                  <h6>May 30, 2024</h6>
+                </div>
+                <div>
+                  <img id="writelogo" src={writeLogo} alt="wite logo" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
